4d3c7c7f6b007f08af3f24aa090c4b9bf2f57021
uros <uros.vardiic@gmail.com>
2020-02-16 20:41:26 +0100

    Initial commit

---
+++ src/main/java/store/server/user/configuration/DatabaseConfiguration.java
@@ -0,0 +1,12 @@
+package store.server.user.configuration;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EntityScan("store.server.user")
+@EnableJpaRepositories("store.server.user")
+public class DatabaseConfiguration {}
